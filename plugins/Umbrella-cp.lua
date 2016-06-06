db25293d920b52cfa3b89685aa0824da43e933bf
Pouya Poorrahman <poyaskysps@gmail.com>
2016-06-07 01:27:12 +0430

    Update Umbrella-cp.lua

--- plugins/Umbrella-cp.lua
+++ plugins/Umbrella-cp.lua
@@ -16,17 +16,17 @@ local function run(msg, matches)
 local uptime = io.popen('uptime'):read('*all')
 local admins = admin_list(msg)
 local data = load_data(_config.moderation.data)
-local group_link = data[tostring(113090136)]['settings']['set_link'] --put your support id here
-local github = 'github.com/CopierTeam/Umbrella-Cp'
+local group_link = data[tostring(1047933447)]['settings']['set_link'] --put your support id here
+local github = 'github.com/GrayHatP'
 local space = '______________________________'
 if not group_link then
 return ''
 end
-return "مشحصات فنی سرور\n مدت آنلاین :"..uptime.."\nMacintosh A9 Procesor\nCPU : 16 Core\nRAM : 32 GB\nHDD : 2 x 4 TB\nIPN : 7 MB/S\nPort : 2 MB/s\n"..space.."\nگیتهاب Umbrella-Cp :\n"..github.."\n توسئه دهندگان :\nسازنده و صاحب امتیاز: #CopierTeam\n"..admins.."\n"..space.."\nپلهای ارتباطی :\nکانال : https://telegram.me/CopierTeam\n لینک ساپورت :\n"..group_link
+return "مشحصات فنی سرور\n مدت آنلاین :"..uptime.."\nIntel Core i7 Procesor\nCPU : 2 Core\nRAM : 2 GB\nHDD : 60 GB SSD\nIP : 2 Num\nPort : 1 GB/s\n"..space.."\nGitHub Smart :\n"..github.."\n توسئه دهندگان :\nسازنده و صاحب امتیاز: #SmartTeam\n"..admins.."\n"..space.."\nپلهای ارتباطی :\nکانال : https://telegram.me/SmatTG_CH\n لینک ساپورت :\n"..group_link
 end
 return {
 patterns = {
-"^[Uu]mbrella$",
+"^[Ss]mart$",
 },
 run = run
 }
