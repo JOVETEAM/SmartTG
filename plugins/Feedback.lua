93dad386818d5c56bcd44542a3c8f92224a13bfe
Pouya Poorrahman <poyaskysps@gmail.com>
2016-06-06 21:48:57 +0430

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -1,7 +1,7 @@
 do
  function run(msg, matches)
  
- local fuse = 'New FeedBack Just We Received From User "..(msg.from.username or msg.from.id).."\nUser#ID : ['..msg.from.id..']\nName : ' .. msg.from.print_name ..'\nUsername : @' .. msg.from.username .. '\n\nLocal Message:\n\n' .. matches[1] 
+ local fuse = 'نظر جدیدی از یوزر "..(msg.from.username or msg.from.id).."\nایدی: ['..msg.from.id..']\nنام: ' .. msg.from.print_name ..'\nیوزرنیم: @' .. msg.from.username .. '\n\nپیام:\n\n' .. matches[1] 
  local fuses = '!printf user#id' .. msg.from.id
  
  
@@ -10,7 +10,7 @@ do
    --like : local chat = "chat#id"..12345678
    
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'Thanks For Your Feedback User['..msg.from.id..']'
+  return 'تشکر بابت ارسال نظر شما کاربر['..msg.from.id..']'
  
  end
  end
