5667949a861166c27e501baf552396528536582e
Pouya Poorrahman <poyaskysps@gmail.com>
2016-06-08 18:37:54 +0430

    Update lock_fwd.lua

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -1,26 +1,26 @@
 do
 
 local function pre_process(msg)
-    
-    —Checking mute
+
+    --Checking mute
     local hash = 'mate:'..msg.to.id
-    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then
+    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(m$
             delete_msg(msg.id, ok_cb, true)
             return "done"
         end
-    
+
         return msg
     end
 
-  
+
 
 
 local function run(msg, matches)
     chat_id =  msg.to.id
-    
+
     if is_momod(msg) and matches[1] == 'lock' then
-      
-            
+
+
                     local hash = 'mate:'..msg.to.id
                     redis:set(hash, true)
                     return "قفل فروارد فعال شد"
