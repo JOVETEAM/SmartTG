8d70c26b0999d3d3d0bb5b5da463cdf275369aea
Pouya Poorrahman <poyaskysps@gmail.com>
2016-07-21 01:54:12 +0430

    Update salams.lua

--- plugins/salams.lua
+++ plugins/salams.lua
@@ -2,10 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'Salm babyii🌪⚡️
-            💋
-💋🤖💋
-   💋'
+local text = 'Salm babyii'
 if matches[1] == 'سلام پسرم' or 'سلام' or 'Salm' or 'salam' then
     if is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
